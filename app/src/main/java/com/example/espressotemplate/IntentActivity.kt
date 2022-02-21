3cd577e2e75c279bca013c82a497bbadaedbd7c0
s.fedulov <s.fedulov@corp.vk.com>
2022-02-22 00:32:37 +0300

    Add dialer activity

---
+++ app/src/main/java/com/example/espressotemplate/IntentActivity.kt
@@ -0,0 +1,51 @@
+package com.example.espressotemplate
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.app.ActivityCompat
+
+class IntentActivity : AppCompatActivity() {
+
+    lateinit var phoneInput: EditText
+    lateinit var callButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent)
+
+        phoneInput = findViewById(R.id.editTextPhone)
+        callButton = findViewById(R.id.make_call_button)
+
+        callButton.setOnClickListener {
+            dialPhoneNumber(phoneInput.text.toString())
+        }
+    }
+
+    fun dialPhoneNumber(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_CALL).apply {
+            data = Uri.parse("tel:$phoneNumber")
+        }
+
+        if (ActivityCompat.checkSelfPermission(
+                this@IntentActivity,
+                android.Manifest.permission.CALL_PHONE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this@IntentActivity,
+                arrayOf(android.Manifest.permission.CALL_PHONE),
+                1
+            )
+        } else {
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/espressotemplate/MainActivity.kt
+++ app/src/main/java/com/example/espressotemplate/MainActivity.kt
@@ -4,12 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import com.example.espressotemplate.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var openIntentButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        openIntentButton = findViewById(R.id.dialer_intent_btn)
+
+        openIntentButton.setOnClickListener {
+            openIntentActivity()
+        }
     }
 
     fun openLoginForm(view: View) {
@@ -26,6 +36,12 @@ class MainActivity : AppCompatActivity() {
 
     fun openWebViewActivity(view: View) {
         val intent = Intent(this, WebViewActivity::class.java).apply {
+        }
+        startActivity(intent)
+    }
+
+    private fun openIntentActivity() {
+        val intent = Intent(this, IntentActivity::class.java).apply {
         }
         startActivity(intent)
     }
